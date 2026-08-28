c8fee87a32ddcaf42b8155bf431a3b16a0bfe1d9
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-28 16:33:35 +0500

    add: create input and check if a number is even or odd using shorthand if-else

---
+++ Regular Practice/input & checkevenodd shorthand ifelse.py
@@ -0,0 +1,8 @@
+n=int(input("Enter a number: "))
+
+print("Even") if (n%2==0) else print("Odd")
+
+''' Checking if shorthand method can handle elif
+print("Even") if n%2==0 elif n==0 print("Zero is neither Even nor Odd.") else print("Odd")
+It Doesn't WORK!
+'''
