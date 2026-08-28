80dc4ffeca8e49d017da2903302add622892b58d
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-28 15:51:24 +0500

    add: create 3. Practicing While Loop.py for practicing while loops

---
+++ Regular Practice/3. Practicing While Loop.py
@@ -0,0 +1,14 @@
+
+# Getting a number from the user
+n = int(input("Enter a number: "))
+
+c=1
+while c<=10:
+    print(n,"*",c,"=",c*n)
+    c+=1
+
+# Using a pre-defined number
+n=1
+while n<=10:
+    print(n)
+    n+=1
