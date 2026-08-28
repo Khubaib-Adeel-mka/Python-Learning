f5d1fe83c3c7e0b9333831a6349cca72fed8d52f
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-28 15:50:09 +0500

    add: create 2.py to collect and display user information

---
+++ Regular Practice/2.py
@@ -0,0 +1,14 @@
+name=input("what is your name?")
+age=input("what is your age?")
+classroom=input("what is your class?")
+marks=input("what is your marks?")
+
+# Spacing Starts Here
+print("")
+print("")
+# Spacing Ends Here
+
+print("your name is",name)
+print("your age is",age)
+print("your classroom is",classroom)
+print("your marks is",marks)
