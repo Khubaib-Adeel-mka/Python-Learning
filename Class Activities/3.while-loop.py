4262e6ce5d900e13f8b30352cb66fcae6f3ba082
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-28 15:48:26 +0500

    add: create while-loop.py for class activity on odd numbers

---
+++ Class Activities/3.while-loop.py
@@ -0,0 +1,14 @@
+
+# Class Activity
+# 
+n=2
+odd=0
+while n<=20:
+    # if n%2==0:
+        print(n)
+    # else:
+        odd+=1
+        n+=2
+print("Number of Odds are",odd)
+
+# python while-loop.py
