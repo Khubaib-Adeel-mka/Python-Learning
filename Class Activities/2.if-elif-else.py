636dc08e8a5bd3abf91bb8197ab22c7a4e1c5621
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-28 15:47:52 +0500

    add: implement if-elif-else statement to check number sign

---
+++ Class Activities/2.if-elif-else.py
@@ -0,0 +1,12 @@
+
+# Class Activity
+# Write an if-elif-else statement to check if a number is positive, negative or zero.
+
+n=int(input("Enter a Number: "))
+if n>0:
+    print(n,"is positive")
+elif n<0:
+    print(n,"is negative")
+else:
+    print(n,"is zero")
+
