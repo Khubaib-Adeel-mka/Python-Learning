038da9b4d7e4c191c7d9fbb39221e798ac469dc4
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 15:40:18 +0500

    feat: add list modification example by replacing an item

--- Practice Programming/List Operations/list.py
+++ Practice Programming/List Operations/list.py
@@ -7,3 +7,7 @@ print("List of Some Fruits =",fruits)
 # Accessing List Items
 print("Second fruit in the list =",fruits[1])
 
+# Modifying A List
+fruits[1]="Grapes"
+print("List After Replacing Apple with Grapes =",fruits)
+
