d921a528129e4cd1bdb188737822fda4aa6cbc4d
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 15:09:30 +0500

    refactor: rearrange print statements to include styles function output

--- Regular Practice/Package Structure/main.py
+++ Regular Practice/Package Structure/main.py
@@ -1,13 +1,17 @@
 
 from products import winter_collection
 from products import summer_collection
+from styling import styles
 
 print("We have following Stocks on our Store:")
-
 print("")
 
+print(styles())
+
+print("")
 print("Winter Collection:",winter_collection())
 
 print("Summer Collection:",summer_collection())
-
 print("")
+
+print(styles())
