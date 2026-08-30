3eb29c17c17c2dc8b6cd965d264e0ff83dfbf14d
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 11:54:09 +0500

    add: create initial file with list, tuple, and string examples

---
+++ Class Activities/8.list, tuple, and string.py
@@ -0,0 +1,8 @@
+
+# Class Activity
+# Consider the following list, tuple, and string:
+
+# List: [10, 20, 30, 40, 50, 60, 70, 80]
+# Tuple: ["Math","Science","English","History","Geography"]
+# String: "Python Programming"
+
