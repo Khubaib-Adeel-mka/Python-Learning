c5a6c3be92392ca98e55079f16a7bc02adc03545
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 10:22:06 +0500

    fix: correct indentation and comments in the even and odd number counting program

--- Class Activities/3.while-loop.py
+++ Class Activities/3.while-loop.py
@@ -1,12 +1,12 @@
 
 # Class Activity
-# 
+# Write a Python Program that prints even number and counts odd numbers from 1 to 20 using while loop.
 n=2
 odd=0
 while n<=20:
-    # if n%2==0:
+    if n%2==0:
         print(n)
-    # else:
+    else:
         odd+=1
         n+=2
 print("Number of Odds are",odd)
