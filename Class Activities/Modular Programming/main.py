f7872ca36e3971551ec6c06359ade9558a660124
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 12:48:12 +0500

    add: requirements for main.py and calculator module functions

--- Class Activities/Modular Programming/main.py
+++ Class Activities/Modular Programming/main.py
@@ -1,3 +1,17 @@
 
 # Class Activity
-# Create a Python module named calculator.py
+# Create a Python module named calculator.py that includes two functions:
+
+# 1. add (a,b) - This function should return the sum of two numbers.
+# 2. subtract (a,b) - This function should return the difference between two numbers.
+
+# Then, a script named main.py imports your calculator module.
+from calculator import adding
+from calculator import subtracting
+
+#  It uses these functions to perform the following operations:
+# Print the result of adding 15 and 8.
+adding(15,8)
+
+# Print the result of subtracting 10 from 25
+subtracting(10,25)
