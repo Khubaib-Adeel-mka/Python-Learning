8b025d9995f4e9f8942ec2b8e9699f024393c821
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 11:55:16 +0500

    add: implement operations to access and print the third element from a list, tuple, and string

--- Class Activities/8.list, tuple, and string.py
+++ Class Activities/8.list, tuple, and string.py
@@ -10,3 +10,9 @@ my_list= [10, 20, 30, 40, 50, 60, 70, 80]
 my_tuple= ["Math","Science","English","History","Geography"]
 my_string= "Python Programming"
 
+# Perform the following operations:
+# Access and print the third element from each sequence (list, tuple, and string).
+print(my_list[2])
+print(my_tuple[2])
+print(my_string[2])
+
