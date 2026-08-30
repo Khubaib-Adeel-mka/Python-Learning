29e20ac107c787c87735e48b6217d142ff30bb8a
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 15:00:25 +0500

    add: implement imports of functions from products module

---
+++ Regular Practice/Package Structure/main.py
@@ -0,0 +1,13 @@
+
+from products import winter_collection
+from products import summer_collection
+
+print("We have following Stocks on our Store:")
+
+print("")
+
+print("Winter Collection:",winter_collection())
+
+print("Summer Collection:",summer_collection())
+
+print("")
