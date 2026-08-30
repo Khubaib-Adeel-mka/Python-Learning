49594f99158472074de710f993fd8dc04f001033
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 14:05:56 +0500

    add: include current date and time functionality in libraries.py

--- Regular Practice/libraries.py
+++ Regular Practice/libraries.py
@@ -4,3 +4,8 @@ import random
 number=random.randint(1,10)
 print("Random Number is", number)
 
+# Print Current Date & Time
+import datetime
+current_time=datetime.datetime.now()
+print("Current Date/Time is",current_time)
+
