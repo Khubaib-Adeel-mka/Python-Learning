91aca7b592441beff0d1e46d6cb3510e46ad8adb
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 14:58:18 +0500

    add: implement summer_collection function in products.py

--- Regular Practice/Package Structure/products.py
+++ Regular Practice/Package Structure/products.py
@@ -2,3 +2,7 @@ def winter_collection():
     items=["Jackets","Gloves","Hoodies","Shirts","Jeans"]
     return items
 
+def summer_collection():
+    items=["Shorts","Polo","Short Dresses","Skirts","Slides","Trousers"]
+    return items
+
