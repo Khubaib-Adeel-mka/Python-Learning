357c321ccc9e5c68ff5051b6393d2fbf15db89cb
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 15:40:49 +0500

    feat: add functionality to append Pomegranate to the fruit list

--- Practice Programming/List Operations/list.py
+++ Practice Programming/List Operations/list.py
@@ -11,3 +11,8 @@ print("Second fruit in the list =",fruits[1])
 fruits[1]="Grapes"
 print("List After Replacing Apple with Grapes =",fruits)
 
+# Adding "Pomegranates" at the End of List
+fruits.append("Pomegranate")
+print("List After adding Pomegranate at the End =",fruits)
+
+print("")
