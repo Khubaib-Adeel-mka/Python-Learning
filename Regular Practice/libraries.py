df8a63d31b9ab7bca3cb423957208ec579e34fa9
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 14:20:10 +0500

    add: implement square root calculation in libraries.py

--- Regular Practice/libraries.py
+++ Regular Practice/libraries.py
@@ -15,3 +15,8 @@ my_list=[1,2,3,4,5,6,7,8,9,10]
 mean=statistics.mean(my_list)
 print("Mode of",my_list,"is",mean)
 
+# Find Square root of a number
+import math
+m=36
+addition=math.sqrt(m)
+print(addition)
