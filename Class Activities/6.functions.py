2c3ecb2ac6c9ca071cf8273131adb71e5f7ef9ea
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 11:06:02 +0500

    add: implement function to return the maximum value from a list of numbers

---
+++ Class Activities/6.functions.py
@@ -0,0 +1,10 @@
+
+# Class Activity
+# Define a function that takes a list of numbers and returns the maximum value.
+
+def return_max_value(numbers):
+    return max(numbers) 
+
+my_list=[1,2,3]
+output=return_max_value(my_list)
+print(output)
