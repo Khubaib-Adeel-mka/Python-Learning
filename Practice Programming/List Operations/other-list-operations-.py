270c493c08004f5fe6df75a073887802a9e8f9b8
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 20:31:21 +0500

    feat: add example of appending an item to a list

--- Practice Programming/List Operations/other-list-operations-.py
+++ Practice Programming/List Operations/other-list-operations-.py
@@ -4,3 +4,10 @@
 # 2. remove(item)
 # 3. sort()
 # 4. reverse()
+
+box_items=["books","novels","stories","dialogues"]
+
+# 1. Add "stationary" at the end.
+box_items.append("stationary")
+print("List after adding stationary and the end =",box_items)
+
