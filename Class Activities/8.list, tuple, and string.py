e2f5ac2c21ed6513aae25807de68f5038dc2e89f
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-08-30 11:54:43 +0500

    add: define list, tuple, and string variables for clarity

--- Class Activities/8.list, tuple, and string.py
+++ Class Activities/8.list, tuple, and string.py
@@ -6,3 +6,7 @@
 # Tuple: ["Math","Science","English","History","Geography"]
 # String: "Python Programming"
 
+my_list= [10, 20, 30, 40, 50, 60, 70, 80]
+my_tuple= ["Math","Science","English","History","Geography"]
+my_string= "Python Programming"
+
