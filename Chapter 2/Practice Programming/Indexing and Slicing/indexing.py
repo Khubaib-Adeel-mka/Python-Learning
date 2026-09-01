4d742cb7cc7dc4816de265ac40a8556fad6cbff7
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-01 21:07:42 +0500

    feat: update print statement and comments for clarity in indexing.py

--- Chapter 2/Practice Programming/Indexing and Slicing/indexing.py
+++ Chapter 2/Practice Programming/Indexing and Slicing/indexing.py
@@ -1,10 +1,21 @@
 
 fruits=["Apple","Banana","Cherry","Date","Elderberry","Grapes"]
 
-print(fruits)       # Output: ["Apple","Banana","Cherry","Date","Elderberry","Grapes"]
+print("Fruits List=",fruits)       # Output: ["Apple","Banana","Cherry","Date","Elderberry","Grapes"]
 
 
-# Calling List Elements by Index Numbers
+# Calling List Elements by Positive Index Numbers
+print("")
+
+print("Calling List Elements by Positive Index Numbers:")
 print(fruits[0])    # Output: Apple
 print(fruits[3])    # Output: Date
 print(fruits[5])    # Output: Grapes
+
+# Calling List Elements by Negative Index Numbers
+print("")
+
+print("Calling List Elements by Negative Index Numbers:")
+print(fruits[-6])   # Output: Apple
+print(fruits[-3])   # Output: Date
+print(fruits[-1])
