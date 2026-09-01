b9fcebe1c94e18f386b39c674a427980732db625
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-01 18:57:40 +0500

    feat: add indexing and slicing example in indexing.py

---
+++ Chapter 2/Practice Programming/Indexing and Slicing/indexing.py
@@ -0,0 +1,10 @@
+
+fruits=["Apple","Banana","Cherry","Date","Elderberry","Grapes"]
+
+print(fruits)       # Output: ["Apple","Banana","Cherry","Date","Elderberry","Grapes"]
+
+
+# Calling List Elements by Index Numbers
+print(fruits[0])    # Output: Apple
+print(fruits[3])    # Output: Date
+print(fruits[5])    # Output: Grapes
