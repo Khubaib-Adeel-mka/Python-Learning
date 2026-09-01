cf62389f4baf82ddf7a169c30e738e4239f822b7
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-01 20:54:55 +0500

    feat: add negative slicing example in slicing.py

--- Chapter 2/Practice Programming/Indexing and Slicing/slicing.py
+++ Chapter 2/Practice Programming/Indexing and Slicing/slicing.py
@@ -3,3 +3,6 @@ fruits=[1,2,3,4,5,6,7,8,9,10]
 # Slicing from Positive Index:
 print("List from Index 2 to 8", fruits[2:9])
 print("List from index 6 to 9",fruits[6:10])
+
+# Slicing from Negative Index:
+print("List from index -2 to -8", fruits[-2:- 8])
