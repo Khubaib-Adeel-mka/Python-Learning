da0935656cf0d220e7329ea340ed43382fc8427f
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-02 18:29:37 +0500

    feat: implement Rectangle class with show_info and calculate_area methods

--- Chapter 2/Practice Programming/Object-Oriented Programming/oop-Rectangle challenge.py
+++ Chapter 2/Practice Programming/Object-Oriented Programming/oop-Rectangle challenge.py
@@ -33,3 +33,23 @@
 
 # The new concept here is return versus print. Don't use print() inside calculate_area().
 
+class Rectangle:
+    def __init__(self, length, width):
+        self.length=length
+        self.width=width
+
+    def show_info(self):
+        print(self.length)
+        print(self.width)
+
+    def calculate_area(self, length,width):
+        print(length*width)
+
+
+length= 10
+width = 5
+
+rectangle1=Rectangle(10, 5)
+rectangle1.show_info()
+
+rectangle1.calculate_area(length,width)
