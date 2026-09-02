eee40a44bea5b3895435d0c97655fa0a8b78f598
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-02 20:30:15 +0500

    fix: correct logic in check_result method to use calculated average for result evaluation

--- Chapter 2/Practice Programming/Object-Oriented Programming/oop-using-return-value.py
+++ Chapter 2/Practice Programming/Object-Oriented Programming/oop-using-return-value.py
@@ -34,3 +34,29 @@
 
 # The important new part is this: one method should call another method of the same object using self.
 
+class Student:
+    def __init__(self,name,marks1,marks2):
+        self.name=name
+        self.marks1=marks1
+        self.marks2=marks2
+
+    def show_info(self):
+        print(self.name)
+        print(self.marks1)
+        print(self.marks2)
+
+    def calculate_average(self):
+        avg=(self.marks1+self.marks2)/2
+        return avg
+    
+    def check_result(self):
+        if self.calcu>=50:
+            print("Pass")
+        else:
+            print("Fail")
+
+student1=Student("Ali",70,50)
+
+average_of_both_marks=student1.calculate_average()
+print(student1.calculate_average())
+student1.check_result()
