2959777c64591af825ea066a2cfb7bcde46a06f1
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-02 15:51:53 +0500

    feat: add initial implementation of Car class with methods for info display and speed check

---
+++ Chapter 2/Practice Programming/Object-Oriented Programming/oop-next challenge.py
@@ -0,0 +1,32 @@
+# Next challenge — slightly harder
+# Create a class named Car.
+
+# It should store:
+
+# brand
+# model
+# speed
+
+# Create two methods:
+
+# show_info()
+# check_speed()
+
+# show_info() should display all three attributes.
+
+# check_speed() should print:
+
+# "Fast"
+
+# if speed is 100 or more, otherwise:
+
+# "Normal"
+
+# Use:
+
+# Brand: Toyota
+# Model: Corolla
+# Speed: 120
+
+# Create one object and call both methods.
+
