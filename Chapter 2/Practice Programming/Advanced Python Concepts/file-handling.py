f9383f2126a4c0b92358d949ff1da3aa82b8ecc1
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-04 22:53:14 +0500

    feat: append new student name to students.txt and update file handling in file-handling.py

--- Chapter 2/Practice Programming/Advanced Python Concepts/file-handling.py
+++ Chapter 2/Practice Programming/Advanced Python Concepts/file-handling.py
@@ -1,6 +1,50 @@
 
 
-with open("students.txt", "r") as file:
-    content=file.read()
+with open("students.txt", "a") as file:
+    content=file.write("Ahmed")
 
-print(content)
+with open("students.txt","r") as file:
+    content=file.read()
+    print(content)
+
+
+
+
+# Suppose you write:
+# name = "Ali"
+# marks = 90
+
+# These values exist while the program is running.
+# But after the program ends, those variables disappear from memory.
+
+# What if you want the marks to remain stored?
+
+# You need a file.
+# For example:
+
+# students.txt
+
+
+
+# Think of it this way:
+
+# Variable
+#    │
+#    │ temporary
+#    ↓
+# Computer Memory (RAM)
+#    │
+#    │ program closes
+#    ↓
+# Data disappears
+
+# But:
+
+# Python Program
+#       │
+#       ↓
+#     File
+# students.txt
+#       │
+#       ↓
+# Data remains stored
